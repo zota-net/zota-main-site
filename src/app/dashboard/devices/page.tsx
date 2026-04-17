842cf1655fbca6bcd8c050b16c2db21724b44e4c
andrua-joshua <andruajoshua096@gmail.com>
2026-04-18 01:09:01 +0300

    ..fixedr the issus of downloading the config

--- src/app/dashboard/devices/page.tsx
+++ src/app/dashboard/devices/page.tsx
@@ -318,7 +318,7 @@ export default function DevicesPage() {
       const url = window.URL.createObjectURL(blob);
       const link = document.createElement('a');
       link.href = url;
-      link.download = `login-config-${routerId}.html`;
+      link.download = `xetihub-hotspot-${routerId}.zip`;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
